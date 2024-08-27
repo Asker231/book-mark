045bfe28053267f7da938d9ecac67c2951c9ea74
Asker <kotsevasker0@gmail.com>
2024-08-27 23:45:40 +0300

    add new funcs

--- main.go
+++ main.go
@@ -11,25 +11,22 @@ import (
 
     func main(){
 		books := BookMark{}
-		result := Menu()
 		loop:
 		for{
+			result := Menu()
 		 switch result{
 			case 1:
-				WatchMark(&books)
-				break loop
+				WatchMark(books)
+				break
 			case 2:
-			    AddMark(&books)
-			break loop
-
+			    AddMark(books)
+				break
 			case 3:
-				DeleteMark(&books)
-				break loop
-
+				DeleteMark(books)
+				break
 			case 4:
 				Out()
 				break loop
-
 			default:
 				fmt.Println("Введите корректные данные")
 				break loop
@@ -42,7 +39,7 @@ import (
 		c := Load()
 		color.Blue("Меню")
 		for range 3{
-			time.Sleep(1 * time.Second)
+			time.Sleep(500 * time.Millisecond)
 			c.Print(" _ ")
 		}
 		fmt.Println("")
@@ -55,25 +52,42 @@ import (
 		return choice
 	}
 
-	func WatchMark(books *BookMark){
-		fmt.Println("wathc")
+	func WatchMark(books BookMark){
+		if len(books) == 0{
+			fmt.Println("Нет закладок")
+		}
+		for key,value := range books{
+			fmt.Println(key + " : " + value)
+		}
 	}
 
-	func AddMark(books *BookMark){
-		fmt.Println("Add")
-	}
+	func AddMark(books BookMark){
+		var key,value string
+		fmt.Print("Введите название: ")
+		fmt.Scan(&key)
+		fmt.Print("Введите ссылку: ")
+		fmt.Scan(&value)
+		books[key] = value
 
-	func DeleteMark(books *BookMark){
-		fmt.Println("Delete")
+	}
+	func DeleteMark(books BookMark){
+		var key string
+		fmt.Print("Введите ключ для удаления: ")
+		fmt.Scan(&key)
+		for k := range books{
+			if k == key{
+				delete(books,k)
+			}
+		}	
 	}
 
 	func Out(){
-		fmt.Println("Out")
+		fmt.Println("Вы вышли")
 	}
 
 
 	func Load() *color.Color{
-		c := color.New(color.FgCyan).Add(color.Underline)
+		c := color.New(color.FgCyan)
 		return c
 	}
 
